7b4fb41796d5e8084fdfec8ae09a3424c5a1472c
Jordi Puiggené <jp@naralabs.com>
2020-12-21 19:22:35 +0100

    Added documentation

---
+++ docs/license.rst
@@ -0,0 +1,21 @@
+License
+=======
+
+**SENAITE.AST** Copyright (C) 2019-2020 RIDING BYTES & NARALABS
+
+`SENAITE.AST`_ is available under the terms of the `GNU General Public
+License, version 2`_ as published by the `Free Software Foundation`_.
+
+The source code of this software, together with a copy of the license can be
+found at this repository: https://github.com/senaite/senaite.ast
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+
+.. Links
+
+.. _SENAITE.AST: https://pypi.python.org/pypi/senaite.ast
+.. _Free Software Foundation: https://www.fsf.org/about/
+.. _GNU General Public License, version 2: https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
---
+++ src/senaite/ast/tests/test_doctests.py
@@ -0,0 +1,34 @@
+
+import doctest
+from os.path import join
+
+from pkg_resources import resource_listdir
+
+import unittest2 as unittest
+from senaite.ast import PRODUCT_NAME
+from senaite.ast.tests.base import BaseTestCase
+from Testing import ZopeTestCase as ztc
+
+# Option flags for doctests
+flags = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE | doctest.REPORT_NDIFF
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    for doctestfile in get_doctest_files():
+        suite.addTests([
+            ztc.ZopeDocFileSuite(
+                doctestfile,
+                test_class=BaseTestCase,
+                optionflags=flags
+            )
+        ])
+    return suite
+
+
+def get_doctest_files():
+    """Returns a list with the doctest files
+    """
+    files = resource_listdir(PRODUCT_NAME, "tests/doctests")
+    files = filter(lambda file_name: file_name.endswith(".rst"), files)
+    return map(lambda file_name: join("doctests", file_name), files)
