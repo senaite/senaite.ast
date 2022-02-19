d8db6be7cdab8f827306aea96ae1ff47f4d9a2a9
Jordi Puiggené <jp@naralabs.com>
2022-02-19 18:28:33 +0100

    Set status to interims on analysis submit

--- src/senaite/ast/workflow/analysis.py
+++ src/senaite/ast/workflow/analysis.py
@@ -19,6 +19,7 @@
 # Some rights reserved, see README and LICENSE.
 
 import copy
+from datetime import datetime
 
 from bika.lims import api
 from bika.lims.interfaces import IAuditable
@@ -30,6 +31,7 @@ from senaite.ast.config import IDENTIFICATION_KEY
 from senaite.ast.config import REPORT_KEY
 from senaite.ast.config import RESISTANCE_KEY
 from senaite.ast.interfaces import IASTAnalysis
+from senaite.core.api import dtime as dt
 from senaite.core.catalog import SETUP_CATALOG
 from zope.interface import alsoProvides
 from zope.interface import noLongerProvides
@@ -67,6 +69,13 @@ def after_submit(analysis):
     values = map(lambda interim: interim.get("value"), interim_fields)
     if not all(values):
         return
+
+    # Update interim fields with status information. This makes a "simulated"
+    # partial entry of results possible: if user adds new antibiotics, the
+    # analysis will rollback to its previous status and new antibiotics will
+    # be added as new interim fields, but existing ones, with an status
+    # attribute, will be rendered in read-only mode
+    update_interim_status(analysis)
 
     # All siblings for same microorganism and sample have to be submitted
     siblings = utils.get_ast_siblings(analysis)
@@ -168,3 +177,18 @@ def copy_interims(source, destination):
     interim_fields = copy.deepcopy(source.getInterimFields())
     map(lambda interim: interim.update({"value": ""}), interim_fields)
     destination.setInterimFields(interim_fields)
+
+
+def update_interim_status(analysis):
+    """Updates interim fields with the analysis status information
+    """
+    status = api.get_review_status(analysis)
+    status_by = "{}_by".format(status)
+    status_date = dt.to_iso_format(datetime.now())
+    user_id = api.get_current_user().id
+    interim_fields = copy.deepcopy(analysis.getInterimFields())
+    for interim_field in interim_fields:
+        interim_field.update({
+            status: status_date,
+            status_by: user_id})
+    analysis.setInterimFields(interim_fields)
