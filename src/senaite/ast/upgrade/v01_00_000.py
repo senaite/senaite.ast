7578898ca4b1466bcddcbf67b1f76b888ff4c562
Jordi Puiggené <jp@naralabs.com>
2022-06-18 13:14:01 +0200

    Code headers

--- src/senaite/ast/upgrade/v01_00_000.py
+++ src/senaite/ast/upgrade/v01_00_000.py
@@ -2,8 +2,8 @@
 #
 # This file is part of SENAITE.AST.
 #
-# SENAITE.AST is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
+# SENAITE.AST is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
 # Software Foundation, version 2.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
@@ -15,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Copyright 2020-2021 by it's authors.
+# Copyright 2020-2022 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
 from bika.lims import api
